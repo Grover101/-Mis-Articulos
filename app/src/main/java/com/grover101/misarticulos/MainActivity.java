295babd1139186c74a56ef1b132c80c1b797866f
Grover101 <limachigrover09@gmail.com>
2022-05-17 15:25:44 -0400

    add: login

--- app/src/main/java/com/grover101/misarticulos/MainActivity.java
+++ app/src/main/java/com/grover101/misarticulos/MainActivity.java
@@ -1,14 +1,50 @@
 package com.grover101.misarticulos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button cerrarSesion = (Button) findViewById(R.id.btn_cerrar_sesion);
+        cerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthUI.getInstance().signOut(MainActivity.this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        MainActivity.this.finish();
+                    }
+                });
+            }
+        });
+
+        Button datosUsuario = (Button) findViewById(R.id.btn_datos_usuario);
+        datosUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lanzarDatosUsuario();
+            }
+        });
+    }
+
+    public void lanzarDatosUsuario(){
+        Intent i = new Intent(this,UsuarioActivity.class);
+        startActivity(i);
     }
 }
